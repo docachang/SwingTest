f4d27751c3c589d925d04bdc85dd8a5806708153
kuma <teraumareninja@gmail.com>
2017-07-23 02:56:54 +0900

    大幅仕様変更 いろいろ間違えてた

---
+++ SwingTest/src/jp/ac/kuma/swingtest/TestWindow.java
@@ -0,0 +1,14 @@
+package jp.ac.kuma.swingtest;
+
+import javax.swing.JFrame;
+
+public class TestWindow extends JFrame {
+
+	TestWindow(){
+		setTitle("TestWindow");
+		setBounds(100,100,600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
